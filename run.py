97b2ba427fdcb4e909c89f3b4973bbeeff95b5b9
TommasoMolesti <tommymolesti246@gmail.com>
2025-08-30 11:30:31 +0200

    Creating a script to clean the environment before testing

---
+++ run.py
@@ -0,0 +1,55 @@
+import subprocess
+import sys
+
+IMAGE_TO_REMOVE = "tommasomolesti/custom_python_heavy:v2" 
+WARMED_CONTAINER_PREFIX = "warmed--"
+
+def run_command(command, stream_output=False):
+    """
+    Esegue un comando di shell. Se stream_output è True, l'output viene
+    mostrato in tempo reale. Altrimenti, viene catturato e mostrato alla fine.
+    """
+    try:
+        if stream_output:
+            subprocess.run(command, shell=True, check=True)
+        else:
+            process = subprocess.run(
+                command, 
+                shell=True, 
+                check=True, 
+                capture_output=True, 
+                text=True
+            )
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante l'esecuzione del comando.")
+
+
+if __name__ == "__main__":
+    print("\nInizio la pulizia dell'ambiente Docker...")
+    print("\n---Fermo i servizi di docker---")
+    run_command("docker-compose down")
+
+    container_ids_str = subprocess.check_output(
+        f'docker ps -a --filter "name={WARMED_CONTAINER_PREFIX}" -q', 
+        shell=True, 
+        text=True
+    ).strip()
+    
+    if container_ids_str:
+        container_ids = container_ids_str.split('\n')
+        print("\n---Fermo i container warmed---")
+        run_command(f"docker stop {' '.join(container_ids)}")
+        print("\n---Rimuovo i container warmed---")
+        run_command(f"docker rm {' '.join(container_ids)}")
+
+
+    print(f"\n---Rimuovo l'immagine '{IMAGE_TO_REMOVE}'---")
+    run_command(f"docker rmi {IMAGE_TO_REMOVE}")
+    
+    print("\n✅ Pulizia dell'ambiente completata.")
+    print("\nAvvio progetto...\n")
+
+    run_command("docker-compose up --build", stream_output=True)
+
+    print("\n\n✅ Test completato.")
